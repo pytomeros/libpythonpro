bc6181c3632a169bb17d62c461de1472fe46ff96
Juliana Raquel <julianamedeiros_228@hotmail.com>
2022-04-26 04:46:04 -0300

    Alterado gestor de dependências2

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -2,9 +2,9 @@ import requests
 
 
 def buscar_avatar(usuario):
-    """ Busca o avatar de um usuário no Github
+    """Busca o avatar de um usuário no Github
     :param usuario: str com o nome de usuario no github
-    : return: str com link do avatar """
+    : return: str com link do avatar"""
 
     url = f'https://api.github.com/users/{usuario}'
     resp = requests.get(url)
